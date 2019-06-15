058180fa140c90bc5e420a3e6d3455c47caca59c
gaxle <g.axler@gmail.com>
2019-06-15 13:03:35 -0400

    "new remote image added
    Updated log_jupyter_example.ipynb"

--- log_jupyter_example.py
+++ log_jupyter_example.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
 # Move this cell UP and ModelCheckpoint will be logged
@@ -16,9 +16,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import cv2
+import urllib
 
 
-# In[2]:
+# In[ ]:
 
 
 from trains import Task
@@ -26,20 +27,35 @@ task = Task.init(project_name="examples", task_name='mnist_train_example')
 logger = task.get_logger()
 
 
-# In[3]:
+# In[ ]:
 
 
 logger.set_default_upload_destination('s3://allegro-tutorials/ComputerVision')
 
 
-# In[4]:
+# In[ ]:
 
 
-task_params = {'num_scatter_samples': 60, 'sin_max_value': 20, 'sin_steps': 30}
+task_params = {
+    # Plots
+    'num_scatter_samples': 60, 'sin_max_value': 20, 'sin_steps': 30,
+    # Mnist MLP
+    'batch_size': 128, 'nb_classes': 10, 'nb_epoch': 6, 'hidden_dim':512
+}
 task_params = task.connect(task_params)
 
 
-# In[5]:
+# In[ ]:
+
+
+task_params['custom_message'] = 'Hey! I can add params to a dict and they will be logged automagically!!'
+print('Checkout the custom message:')
+print('*'*len(task_params['custom_message']))
+print(task_params['custom_message'])
+print('*'*len(task_params['custom_message']))
+
+
+# In[ ]:
 
 
 N = task_params['num_scatter_samples']
@@ -55,9 +71,10 @@ x = np.linspace(0, task_params['sin_max_value'], task_params['sin_steps'])
 y = np.sin(x)
 plt.plot(x, y, 'o', color='black')
 plt.title('Sinus Dots')
+plt.show()
 
 
-# In[6]:
+# In[ ]:
 
 
 import numpy as np
@@ -66,7 +83,7 @@ plt.imshow(m)
 plt.title('test2')
 
 
-# In[7]:
+# In[ ]:
 
 
 import logging
@@ -74,16 +91,27 @@ logging.getLogger('mine').warning('testing one two 3 4')
 task.get_logger().flush()
 
 
-# In[8]:
+# In[ ]:
 
 
-task_params['batch_size'] = 128
-task_params['nb_classes'] = 10
-task_params['nb_epoch'] = 6
-task_params['hidden_dim'] = 512
-batch_size = int(task_params['batch_size'])
-nb_classes = int(task_params['nb_classes'])
-nb_epoch = int(task_params['nb_epoch'])
+def url_to_image(url):
+  resp = urllib.request.urlopen(url)
+  image = np.asarray(bytearray(resp.read()), dtype="uint8")
+  image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+  return image
+
+m = url_to_image('https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Altamira_bisons.jpg/495px-Altamira_bisons.jpg')
+task.get_logger().report_image_and_upload("debug", "we-can-log-images", iteration=1, matrix=m)
+
+
+# ### Train an MNIST MLP
+
+# In[ ]:
+
+
+batch_size = task_params['batch_size']
+nb_classes = task_params['nb_classes']
+nb_epoch = task_params['nb_epoch']
 
 
 # the data, shuffled and split between train and test sets
@@ -102,7 +130,7 @@ print(X_test.shape[0], 'test samples')
 Y_train = np_utils.to_categorical(y_train, nb_classes)
 Y_test = np_utils.to_categorical(y_test, nb_classes)
 
-hidden_dim = int(task_params['hidden_dim'])
+hidden_dim = task_params['hidden_dim']
 model = Sequential()
 model.add(Dense(hidden_dim, input_shape=(784,)))
 model.add(Activation('relu'))
@@ -132,32 +160,10 @@ print('Test score:', score[0])
 print('Test accuracy:', score[1])
 
 
-# In[9]:
-
-
-m = cv2.imread('D:\\data\\google_img_scrape\\3d_street_art\\img_17.jpg')
-task.get_logger().report_image_and_upload("debug", "3d-street-art", iteration=1, matrix=m)
-
-
-# In[10]:
+# In[ ]:
 
 
 task.get_logger().flush()
 task.close()
 print('We are done :)')
-
-
-# In[11]:
-
-
-logger.flush()
-
-
-# In[12]:
-
-
-import keras
-import tensorflow
-print('Keras', keras.__version__)
-print('TensorFlow', tensorflow.__version__)
 
